f970e879a519edb49883c82f85edc96dfb52d61c
anastasiya2001 <nastya.shulikina@yandex.ru>
2024-02-26 16:54:54 +0400

    task-1. Add exercise read

---
+++ src/main/java/ru/ssu/task1/Read.java
@@ -0,0 +1,28 @@
+package main.java.ru.ssu.task1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Read {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String filename = scanner.nextLine();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("main/java/ru/ssu/task1/"+filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length >= 3) {
+                    String initials = parts[1] + " " + parts[0].charAt(0) + "." + parts[2].charAt(0) + ".";
+                    System.out.println(parts[1]);
+                    System.out.println(initials);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Еrror reading file: " + e.getMessage());
+        }
+    }
+}
