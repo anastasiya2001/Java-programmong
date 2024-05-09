60c8dec1347c8a8f868f632ef319911b1f0ddd4b
anastasiya2001 <nastya.shulikina@yandex.ru>
2024-05-09 19:03:21 +0400

    task-4. Fix ex 3 (ExEnum)

--- src/main/java/ru/ssu/task4/ExEnum.java
+++ src/main/java/ru/ssu/task4/ExEnum.java
@@ -1,37 +1,49 @@
 package main.java.ru.ssu.task4;
+
 import java.util.Random;
 
 public class ExEnum {
-    enum MyString {
-        INSTANCE;
-        
-        private final StringBuilder stringBuilder = new StringBuilder();
+    public static void main(String[] args) {
+        Random random = new Random();
 
-        public void appendRandomString() {
-            Random random = new Random();
-            StringBuilder randomString = new StringBuilder();
-            for (int i = 0; i < 10; i++) {
-                randomString.append((char) (random.nextInt(26) + 'a'));
-            }
-            stringBuilder.append(randomString.toString());
+        // StringBuilder
+        long startTimeStringBuilder = System.nanoTime();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 100000; i++) {
+            stringBuilder.append(generateRandomString(random, 10));
         }
+        String stringBuilderResult = stringBuilder.toString();
+        long endTimeStringBuilder = System.nanoTime();
+        long durationStringBuilder = (endTimeStringBuilder - startTimeStringBuilder) / 1000000; // Время в миллисекундах
+        System.out.println("Time taken by StringBuilder: " + durationStringBuilder + " milliseconds");
 
-        public String getString() {
-            return stringBuilder.toString();
+        // StringBuffer
+        long startTimeStringBuffer = System.nanoTime();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < 100000; i++) {
+            stringBuffer.append(generateRandomString(random, 10));
         }
+        String stringBufferResult = stringBuffer.toString();
+        long endTimeStringBuffer = System.nanoTime();
+        long durationStringBuffer = (endTimeStringBuffer - startTimeStringBuffer) / 1000000; // Время в миллисекундах
+        System.out.println("Time taken by StringBuffer: " + durationStringBuffer + " milliseconds");
+
+        // String
+        long startTimeString = System.nanoTime();
+        String result = "";
+        for (int i = 0; i < 100000; i++) {
+            result += generateRandomString(random, 10);
+        }
+        long endTimeString = System.nanoTime();
+        long durationString = (endTimeString - startTimeString) / 1000000; // Время в миллисекундах
+        System.out.println("Time taken by String: " + durationString + " milliseconds");
     }
 
-    public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-
-        for (int i = 0; i < 100000; i++) {
-            MyString.INSTANCE.appendRandomString();
+    public static String generateRandomString(Random random, int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append((char) (random.nextInt(26) + 'a'));
         }
-
-        String finalString = MyString.INSTANCE.getString();
-        System.out.println(finalString);
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("Execution time: " + (endTime - startTime) + " milliseconds");
+        return sb.toString();
     }
 }
